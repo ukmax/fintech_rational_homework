5104b600e807e539fa05098a9d61471212c6b438
ukmax <tealiss@ya.ru>
2019-10-26 19:55:49 +0300

    add testLess()

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,4 +8,11 @@ public class Tests {
         assertEquals("Standard constructor returns wrong numerator", 0, standard.getNumerator());
         assertEquals("Standard constructor returns wrong denominator", 1, standard.getDenominator());
     }
+
+    @Test
+    public void testLess(){
+        Rational r1 = new Rational(2, 4);
+        Rational r2 = new Rational(3,9);
+        assertTrue("Equal or greater, not less", r2.less(r1));
+    }
 }
